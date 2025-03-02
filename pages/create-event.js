f4c0e719af67e969ea1ebd547272d99693dac4ec
FetuStudio <fetu.studio@encantia.lat>
2025-03-02 01:20:47 +0100

    Update create-event.js

--- pages/create-event.js
+++ pages/create-event.js
@@ -93,13 +93,6 @@ export default function UserArea() {
                         >
                             Crear Libro
                         </button>
-                    {role === 'owner, admin' && (
-                        <button
-                            className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                            onClick={() => router.push('/create-event')}
-                        >
-                            Crear Evento
-                        </button>
 
                     {role === 'owner, admin' && (
                         <button
@@ -108,7 +101,7 @@ export default function UserArea() {
                         >
                             Crear Evento
                         </button>
-
+                     
                     )}
                 </div>
             </div>
