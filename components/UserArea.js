4316dd3ca0359eb67d90836f1e5969d9278ea7c2
FetuStudio <fetu.studio@encantia.lat>
2025-03-02 17:09:08 +0100

    Update UserArea.js

--- components/UserArea.js
+++ components/UserArea.js
@@ -89,7 +89,7 @@ export default function UserArea() {
                     {/* Botón de "Discord" */}
                     <button
                         onClick={() => window.open("https://discord.gg/dxcX8S3mrF", "_blank")}
-                        className="px-4 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-400 transition-colors"
+                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
                     >
                         Discord
                     </button>
