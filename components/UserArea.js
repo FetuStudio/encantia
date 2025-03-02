a9d004ba145c4010fdc1cebf4c78fcca4b39a091
FetuStudio <fetu.studio@encantia.lat>
2025-03-02 01:26:52 +0100

    Update UserArea.js

--- components/UserArea.js
+++ components/UserArea.js
@@ -94,7 +94,7 @@ export default function UserArea() {
                             Crear Libro
                         </button>
 
-                    {role === 'owner, admin' && (
+                    {(role === 'owner' || role === 'admin') && (
                         <button
                             className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
                             onClick={() => router.push('/create-event')}
