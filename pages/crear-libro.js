223f3d97d9346fec8cbdd9737445e705e7400849
FetuStudio <fetu.studio@encantia.lat>
2025-03-02 01:25:19 +0100

    Update crear-libro.js

--- pages/crear-libro.js
+++ pages/crear-libro.js
@@ -150,7 +150,7 @@ export default function CrearLibros() {
                             className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
                             onClick={() => router.push('/crear-libro')}
                         >
-                            Crear Libro
+                            Crear Evento
                         </button>
                      
                     )}
