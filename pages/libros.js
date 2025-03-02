44de11737c2db1126f510cd904d6ef7de454e7b0
FetuStudio <fetu.studio@encantia.lat>
2025-03-02 01:17:50 +0100

    Update libros.js

--- pages/libros.js
+++ pages/libros.js
@@ -69,6 +69,14 @@ export default function Libros() {
                     >
                         Crear Libro
                     </button>
+
+                    {role === 'owner, admin' && (
+                        <button
+                            className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
+                            onClick={() => router.push('/create-event')}
+                        >
+                            Crear Evento
+                        </button>
                 </div>
             </div>
 
