8a9fbab0c7a00426648bca0cd8fb34488f00a2a2
FetuStudio <fetu.studio@encantia.lat>
2025-03-02 17:07:18 +0100

    Update libros.js

--- pages/libros.js
+++ pages/libros.js
@@ -88,6 +88,14 @@ export default function Libros() {
                         Libros
                     </button>
 
+                    {/* Botón de "Discord" */}
+                    <button
+                        onClick={() => window.open("https://discord.gg/dxcX8S3mrF", "_blank")}
+                        className="px-4 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-400 transition-colors"
+                    >
+                        Discord
+                    </button>
+
                     {/* Mostrar botones para "Crear Libro" si el rol es 'owner' */}
                     {role === 'owner' && (
                         <button
