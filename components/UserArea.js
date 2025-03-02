678c1e36ac3e31328e676bdd65d7a6331103f5d8
FetuStudio <fetu.studio@encantia.lat>
2025-03-02 01:09:41 +0100

    Update UserArea.js

--- components/UserArea.js
+++ components/UserArea.js
@@ -64,14 +64,14 @@ export default function UserArea() {
                     {/* Botón de "Chat" */}
                     <button
                         onClick={() => router.push('/chat')}
-                        className="px-4 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-400 transition-colors"
+                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
                     >
                         Chat
                     </button>
 
                     {/* Botón de "Libros" */}
                     <button
-                        className="p-2 bg-gray-700 text-white rounded"
+                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
                         onClick={() => router.push('/libros')}
                     >
                         Libros
@@ -79,7 +79,7 @@ export default function UserArea() {
 
                     {role === 'owner' && (
                         <button
-                            className="p-2 bg-green-600 text-white rounded"
+                            className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
                             onClick={() => router.push('/crear-libro')}
                         >
                             Crear Libro
