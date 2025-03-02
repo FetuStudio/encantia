b7c198271977fc7d46e7c782eb4677a14ab449ad
FetuStudio <fetu.studio@encantia.lat>
2025-03-02 11:58:25 +0100

    Update libros.js

--- pages/libros.js
+++ pages/libros.js
@@ -49,7 +49,7 @@ export default function Libros() {
             <div className="flex justify-between items-center mb-4">
                 <div>
                     <img
-                        src="https://i.ibb.co/933TjLds/encantia-logo-2025.webp"
+                        src="https://images.encantia.lat/encantia-logo-2025.webp"
                         alt="Logo"
                         className="h-16"
                     />
