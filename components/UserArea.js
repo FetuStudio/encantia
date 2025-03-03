c7bd0a56c303cf73e388b26c3cef00653d98e676
FetuStudio <fetu.studio@encantia.lat>
2025-03-03 02:06:42 +0100

    Update UserArea.js

--- components/UserArea.js
+++ components/UserArea.js
@@ -93,6 +93,12 @@ export default function Libros() {
                     >
                         Discord
                     </button>
+                    <button
+                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
+                        onClick={() => router.push('/fg2')}
+                    >
+                        Fetu Games 2
+                    </button>
                     {role === 'owner' && (
                         <button
                             className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
