b5a6317b6bdbeca7890327b54fc95d0b9ac6d401
FetuStudio <fetu.studio@encantia.lat>
2025-03-07 23:02:44 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,13 @@
 // pages/_app.js
 import '../styles/globals.css';  // Importa tu archivo CSS global aquí
 import { useEffect } from 'react';
+import Monitor from "@/components/Monitor";  // Asegúrate de que la ruta sea correcta
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     // Este hook de efecto asegura que el CSS se cargue correctamente
   }, []);
-  
-  return <Component {...pageProps} />;
 
-  import Monitor from "@/components/Monitor";
-
-function MyApp({ Component, pageProps }) {
   return (
     <>
       <Monitor />
