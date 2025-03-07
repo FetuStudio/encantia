c55152688e5f24c916a744df4ae15a6697f27c29
FetuStudio <fetu.studio@encantia.lat>
2025-03-07 23:25:09 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,39 @@
 import '../styles/globals.css'; // Importa tus estilos globales
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import MaintenancePage from './maintenance'; // Importa la página de mantenimiento
+import supabase from '../utils/supabaseClient'; // Importa la configuración function MyApp({ Component, pageProps }) {
 
-function MyApp({ Component, pageProps }) {
+  const [isMaintenance, setIsMaintenance] = useState(false);
+  const [reason, setReason] = useState('');
+  const [startTime, setStartTime] = useState('');
+
   useEffect(() => {
-    // Puedes incluir aquí lógica si necesitas algo en el momento de la carga de la app
+    const fetchMaintenanceStatus = async () => {
+      const { data, error } = await supabase
+        .from('maintenance')
+        .select('is_active, reason, start_time')
+        .eq('id', 1) // Asumiendo que el ID de la fila de mantenimiento es 1
+        .single();
+
+      if (error) {
+        console.error("Error fetching maintenance status:", error);
+      } else {
+        if (data.is_active) {
+          setIsMaintenance(true);
+          setReason(data.reason || 'Mantenimiento programado');
+          setStartTime(data.start_time);
+        }
+      }
+    };
+
+    fetchMaintenanceStatus();
   }, []);
 
-  return <Component {...pageProps} />; // Renderiza el componente de la página
+  if (isMaintenance) {
+    return <MaintenancePage reason={reason} startTime={startTime} />;
+  }
+
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
