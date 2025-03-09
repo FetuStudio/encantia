8cc10a036eff8af855dc24285a695198e17cd698
FetuStudio <fetu.studio@encantia.lat>
2025-03-09 12:53:01 +0100

    Update UserArea.js

--- components/UserArea.js
+++ components/UserArea.js
@@ -98,13 +98,7 @@ export default function Navbar() {
                     >
                         Fetu Games 2
                     </button>
-                    {role === 'owner' && (
-                        <button
-                            className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                            onClick={() => router.push('/crear-libro')}
-                        >
-                            Crear Libro
-                        </button>
+    
                     )}
                 </div>
 
