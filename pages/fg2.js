daefe964d5dd0b66d3f07dc3e6647017bba01655
FetuStudio <fetu.studio@encantia.lat>
2025-03-13 15:05:28 +0100

    Update fg2.js

--- pages/fg2.js
+++ pages/fg2.js
@@ -2,19 +2,19 @@ import { useState, useEffect } from "react";
 import { supabase } from "../utils/supabaseClient"; 
 import { useRouter } from "next/router"; 
 
-export default function Libros() {
+export default function Navbar() {
     const [role, setRole] = useState("");  // Estado para almacenar el rol del usuario
     const [event, setEvent] = useState(null);  // Estado para almacenar el evento
     const [timeLeft, setTimeLeft] = useState("");  // Estado para almacenar el tiempo restante
+    const [showMenu, setShowMenu] = useState(false); // Estado para controlar el menú desplegable
+    const [userProfile, setUserProfile] = useState(null); // Estado para el perfil del usuario
+    const [showLogoutModal, setShowLogoutModal] = useState(false);
     const router = useRouter(); 
 
     useEffect(() => {
-        const fetchUserRole = async () => {
+        const fetchUserProfile = async () => {
             const { data: { user }, error: authError } = await supabase.auth.getUser();
-            if (authError || !user) {
-                console.error("Error al obtener usuario:", authError);
-                return;
-            }
+            if (authError || !user) return;
 
             const { data, error } = await supabase
                 .from('user_roles')
@@ -22,61 +22,71 @@ export default function Libros() {
                 .eq('user_id', user.id)
                 .single();
 
-            if (error) {
-                console.error("Error al obtener el rol del usuario:", error);
-            } else {
+            if (!error) {
                 setRole(data?.role);
             }
-        };
 
-        const fetchEvent = async () => {
-            const { data, error } = await supabase
-                .from("events_fg2")
-                .select("*")
-                .order("event_date", { ascending: true })
-                .limit(1)
+            // Obtener perfil del usuario
+            const { data: profileData, error: profileError } = await supabase
+                .from('profiles')
+                .select('avatar_url')
+                .eq('id', user.id)
                 .single();
 
-            if (error) {
-                console.error("Error al obtener el evento:", error);
-            } else {
-                setEvent(data);
-                calculateTimeLeft(data?.event_date);
+            if (!profileError) {
+                setUserProfile(profileData);
             }
         };
 
-        fetchUserRole();
-        fetchEvent();
-
-        const intervalId = setInterval(() => {
-            if (event) {
-                calculateTimeLeft(event?.event_date);
+        const fetchEvents = async () => {
+            const { data, error } = await supabase.from('events').select('*');
+            if (!error && data && data.length > 0) {
+                // Si hay eventos, tomamos el primero (o el evento actual que te interese)
+                const upcomingEvent = data[0];  // Suponiendo que el primer evento es el más cercano
+                setEvent(upcomingEvent);
+                calculateTimeLeft(upcomingEvent);
             }
-        }, 1000); // Actualiza el contador cada segundo
+        };
 
-        return () => clearInterval(intervalId); // Limpiar el intervalo al desmontar el componente
+        const calculateTimeLeft = (event) => {
+            const eventDate = new Date(event.event_date); // Suponiendo que el campo 'event_date' existe en tu base de datos
+            const now = new Date();
+            const timeDiff = eventDate - now;
 
-    }, [event]);
+            if (timeDiff > 0) {
+                const hours = Math.floor(timeDiff / (1000 * 60 * 60));
+                const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+                setTimeLeft(`${hours} horas y ${minutes} minutos`);
+            } else {
+                setTimeLeft("El evento ha comenzado");
+            }
+        };
 
-    const calculateTimeLeft = (eventDate) => {
-        const eventTime = new Date(eventDate).getTime();
-        const currentTime = new Date().getTime();
-        const timeDiff = eventTime - currentTime;
+        fetchUserProfile();
+        fetchEvents();
+    }, []);
 
-        if (timeDiff <= 0) {
-            setTimeLeft("El evento ya ha comenzado o ha pasado");
-        } else {
-            const months = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 30)); // Aproximado a 30 días por mes
-            const days = Math.floor((timeDiff % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
-            const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
-            setTimeLeft(`${months} meses ${days} días ${hours}h ${minutes}m ${seconds}s`);
-        }
+    const handleLogout = () => setShowLogoutModal(true);
+
+    const confirmLogout = async () => {
+        await supabase.auth.signOut();
+        router.push("/");
     };
 
+    const toggleMenu = () => setShowMenu(!showMenu);
+
+    const navButtons = [
+        { name: 'Inicio', url: 'https://www.encantia.lat/' },
+        { name: 'Eventos', url: '/EventsArea' },
+        { name: 'Chat', url: '/chat' },
+        { name: 'Libros', url: '/libros' },
+        { name: 'Discord', url: 'https://discord.gg/dxcX8S3mrF', target: '_blank' },
+        { name: 'Fetu Games 2', url: '/fg2' }
+    ];
+
     return (
-        <div className="relative flex flex-col h-screen p-4 bg-gray-900 text-white">
+        <div className="flex flex-col h-screen p-4 bg-gray-900 text-white relative">
+            {/* Barra de navegación superior */}
             <div className="flex justify-between items-center mb-4">
                 <div>
                     <img
@@ -87,56 +97,82 @@ export default function Libros() {
                 </div>
 
                 <div className="flex gap-4">
-                    <button
-                        onClick={() => window.location.href = "https://www.encantia.lat/"}
-                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                    >
-                        Inicio
-                    </button>
-                    <button
-                        onClick={() => router.push('/EventsArea')}
-                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                    >
-                        Eventos
-                    </button>
-                    <button
-                        onClick={() => router.push('/chat')}
-                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                    >
-                        Chat
-                    </button>
-                    <button
-                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                        onClick={() => router.push('/libros')}
-                    >
-                        Libros
-                    </button>
-                    <button
-                        onClick={() => window.open("https://discord.gg/dxcX8S3mrF", "_blank")}
-                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                    >
-                        Discord
-                    </button>
-                    <button
-                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                        onClick={() => router.push('/fg2')}
-                    >
-                        Fetu Games 2
-                    </button>
-                    {role === 'owner' && (
+                    {navButtons.map((button, index) => (
                         <button
+                            key={index}
+                            onClick={() => window.open(button.url, button.target || "_self")}
                             className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                            onClick={() => router.push('/crear-libro')}
                         >
-                            Crear Libro
+                            {button.name}
                         </button>
-                    )}
+                    ))}
                 </div>
+
+                {/* Foto de perfil */}
+                {userProfile && (
+                    <div className="relative">
+                        <img
+                            src={userProfile.avatar_url || 'https://i.ibb.co/d0mWy0kP/perfildef.png'}
+                            alt="Avatar"
+                            className="w-12 h-12 rounded-full cursor-pointer"
+                            onClick={toggleMenu}
+                        />
+
+                        {/* Menú desplegable */}
+                        {showMenu && (
+                            <div className="absolute right-0 mt-2 w-48 bg-gray-800 text-white rounded-lg shadow-lg z-10">
+                                <ul className="py-2">
+                                    <li
+                                        className="px-4 py-2 cursor-pointer hover:bg-gray-700"
+                                        onClick={() => router.push('/settings')}
+                                    >
+                                        Configuración
+                                    </li>
+                                    <li
+                                        className="px-4 py-2 cursor-pointer hover:bg-gray-700"
+                                        onClick={() => router.push('/profile')}
+                                    >
+                                        Perfil
+                                    </li>
+                                    <li
+                                        className="px-4 py-2 text-red-500 cursor-pointer hover:bg-gray-700"
+                                        onClick={handleLogout}
+                                    >
+                                        Cerrar sesión
+                                    </li>
+                                </ul>
+                            </div>
+                        )}
+                    </div>
+                )}
             </div>
+
+            {/* Modal de Logout */}
+            {showLogoutModal && (
+                <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 backdrop-blur-md">
+                    <div className="bg-gray-900 text-white p-5 rounded-lg shadow-2xl text-center">
+                        <p className="mb-4 text-lg font-semibold">¿Seguro que quieres cerrar sesión?</p>
+                        <div className="flex justify-center gap-4">
+                            <button
+                                onClick={confirmLogout}
+                                className="px-5 py-2 bg-red-500 text-white rounded-lg hover:bg-red-400 transition-all"
+                            >
+                                Sí
+                            </button>
+                            <button
+                                onClick={() => setShowLogoutModal(false)}
+                                className="px-5 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-500 transition-all"
+                            >
+                                No
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
 
             {/* Mostrar el evento y el contador de tiempo */}
             {event && (
-                <div className="text-center">
+                <div className="text-center mt-8">
                     <h2 className="text-2xl font-bold mb-2">Evento: {event.event_name}</h2>
                     <p className="text-xl">Faltan: {timeLeft}</p>
                 </div>
