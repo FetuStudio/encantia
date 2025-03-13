f7e04e0e155e2447fbabc17229f34405e475f3fc
FetuStudio <fetu.studio@encantia.lat>
2025-03-13 18:40:38 +0100

    Update libros.js

--- pages/libros.js
+++ pages/libros.js
@@ -106,12 +106,6 @@ export default function Libros() {
                     >
                         Discord
                     </button>
-                    <button
-                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                        onClick={() => router.push('/fg2')}
-                    >
-                        Fetu Games 2
-                    </button>
                 </div>
 
                 {userProfile && (
