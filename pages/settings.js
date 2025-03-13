cfa0961df93206452dd32786c920aac0a5580233
FetuStudio <fetu.studio@encantia.lat>
2025-03-13 18:41:27 +0100

    Update settings.js

--- pages/settings.js
+++ pages/settings.js
@@ -188,12 +188,6 @@ export default function Settings() {
                     >
                         Discord
                     </button>
-                    <button
-                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                        onClick={() => router.push('/fg2')}
-                    >
-                        Fetu Games 2
-                    </button>
                 </div>
 
                 {/* Foto de perfil en la parte superior derecha */}
