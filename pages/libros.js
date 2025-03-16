366de6c53d270ae54583fd96d4b3a6fd8499c508
FetuStudio <contacto@encantia.lat>
2025-03-16 12:36:02 +0100

    Update libros.js

--- pages/libros.js
+++ pages/libros.js
@@ -66,12 +66,6 @@ export default function Libros() {
                         Eventos
                     </button>
                     <button
-                        onClick={() => router.push('/chat')}
-                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                    >
-                        Chat
-                    </button>
-                    <button
                         className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
                         onClick={() => router.push('/libros')}
                     >
