df72501f2b30d8da8366dc272438dd015b89d8ba
Encantia <panel@encantia.lat>
2025-06-05 22:22:56 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,35 @@
 import { useEffect } from 'react';
+import Script from 'next/script';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-useEffect(() => {
-  if (typeof window !== "undefined" && "OneSignal" in window) {
-    window.OneSignalDeferred = window.OneSignalDeferred || [];
-    window.OneSignalDeferred.push(function(OneSignal) {
-      OneSignal.init({
-        appId: "97df49d1-b67d-490b-9157-9eea6ff9a278",
-        serviceWorkerPath: "/OneSignalSDK.sw.js", // IMPORTANTE
-        serviceWorkerUpdaterPath: "/OneSignalSDKUpdaterWorker.js",
-        serviceWorkerWorkerPath: "/OneSignalSDKWorker.js"
+  useEffect(() => {
+    if (typeof window !== "undefined" && "OneSignal" in window) {
+      window.OneSignalDeferred = window.OneSignalDeferred || [];
+      window.OneSignalDeferred.push(function(OneSignal) {
+        OneSignal.init({
+          appId: "97df49d1-b67d-490b-9157-9eea6ff9a278",
+          serviceWorkerPath: "/OneSignalSDK.sw.js",
+          serviceWorkerUpdaterPath: "/OneSignalSDKUpdaterWorker.js",
+          serviceWorkerWorkerPath: "/OneSignalSDKWorker.js"
+        });
       });
-    });
-  }
-}, []);
+    }
+  }, []);
 
-
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://web.cmp.usercentrics.eu/modules/autoblocker.js"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="usercentrics-cmp"
+        src="https://web.cmp.usercentrics.eu/ui/loader.js"
+        data-settings-id="E0yLicy4fkb4pH"
+        strategy="afterInteractive"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
