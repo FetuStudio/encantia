b31b79bddd34f052c6e0a758962f2d43447a9be8
Encantia <panel@encantia.lat>
2025-06-18 23:48:05 +0200

    Update profiles.js

--- pages/profiles.js
+++ pages/profiles.js
@@ -46,7 +46,7 @@ export default function Navbar() {
     const navButtons = [
     { icon: "https://images.encantia.lat/home.png", name: "Inicio", url: '/' },
     { icon: "https://images.encantia.lat/libros.png", name: "Libros", url: '/libros' },
-    { icon: "https://images.encantia.lat/eventos.png", name: "Eventos", url: '/events' },
+    { icon: "https://images.encantia.lat/eventos.png", name: "Eventos", url: '/events },
     { icon: "https://images.encantia.lat/music.png", name: "Musica", url: '/music' },
     { icon: "https://images.encantia.lat/users2.png", name: "Usuarios", url: '/profiles' },
     { icon: "https://images.encantia.lat/discord.png", name: "Discord", url: 'https://discord.gg/BRqvv9nWHZ' }
